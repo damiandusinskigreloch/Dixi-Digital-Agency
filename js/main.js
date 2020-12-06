bf88975099a2e21ffecb5e60907173dec24f022b
damiandusinskigreloch <damiandusinskiggreloch@gmail.com>
2020-12-06 20:25:37 +0100

    add class for text in initiative-box and create  accordion effect

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+// Production JS :)
+
 document.querySelector('.nav-btn').addEventListener('click', () => {
   document.querySelector('.main-nav').classList.add('show-menu');
 })
 document.querySelector('.close-btn').addEventListener('click', () => {
   document.querySelector('.main-nav').classList.remove('show-menu');
+})
+
+var x = document.querySelectorAll('.btn-show-more');
+x.forEach(function (item) {
+  item.addEventListener('click', () => {
+    item.parentNode.querySelector('.text-more').classList.toggle('show');
+  })
 })
