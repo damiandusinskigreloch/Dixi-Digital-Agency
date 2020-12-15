437548f03a802d905dc6f3f7189a60e0730f2af2
damiandusinskigreloch <damiandusinskiggreloch@gmail.com>
2020-12-15 20:37:02 +0100

    delete old css file

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ navBtn.addEventListener('click', () => {
   mainNav.classList.toggle('menu--show');
 })
 
-
-
 btnShowMore.forEach(function (btn) {
   btn.addEventListener('click', () => {
     btn.parentNode.querySelector('.raport__text-more').classList.toggle("raport__text-more--show");
