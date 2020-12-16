8e7743e987219c7263023bbcd924cd099cdb4ea4
damiandusinskigreloch <damiandusinskiggreloch@gmail.com>
2020-12-16 21:11:33 +0100

    add  media for sm and md screen

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let navBtn = document.querySelector('.top-bar__btn-container');
 let mainNav = document.querySelector('.menu');
 let btnShowMore = document.querySelectorAll('.raport__btn-show-more');
-
+let offerBtn = document.querySelector('.menu__link--disable');
 navBtn.addEventListener('click', () => {
   mainNav.classList.toggle('menu--show');
 })
@@ -11,4 +11,11 @@ btnShowMore.forEach(function (btn) {
   btn.addEventListener('click', () => {
     btn.parentNode.querySelector('.raport__text-more').classList.toggle("raport__text-more--show");
   })
+})
+
+
+offerBtn.addEventListener('click', (e) => {
+e.preventDefault();
+console.log(e.target.nextElementSibling)
+e.target.nextElementSibling.classList.toggle('menu__list-inbox-active')
 })
