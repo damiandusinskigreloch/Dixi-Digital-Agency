0ad59719f52d7116e48e532cde2e00b6a605e509
damiandusinskigreloch <damiandusinskiggreloch@gmail.com>
2020-12-18 15:19:26 +0100

    change menu and fix possibilities on small screen

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 // Production JS :)
+function noScroll() {
+  window.scrollTo(0, 0);
+}
+
+// add listener to disable scroll
+window.addEventListener('scroll', noScroll);
+
+// Remove listener to re-enable scroll
+window.removeEventListener('scroll', noScroll);
+
+// 
 let navBtn = document.querySelector('.top-bar__btn-container');
 let mainNav = document.querySelector('.menu');
 let btnShowMore = document.querySelectorAll('.raport__btn-show-more');
 let offerBtn = document.querySelector('.menu__link--disable');
 navBtn.addEventListener('click', () => {
   mainNav.classList.toggle('menu--show');
+  window.addEventListener('scroll', noScroll);
+  if(mainNav.className == "menu"){
+    window.removeEventListener('scroll', noScroll);
+
+
+  }
 })
 
 btnShowMore.forEach(function (btn) {
@@ -19,3 +36,4 @@ e.preventDefault();
 console.log(e.target.nextElementSibling)
 e.target.nextElementSibling.classList.toggle('menu__list-inbox-active')
 })
+
